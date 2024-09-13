d4682ced0ef538ab3e6b6ed9d483745cd37c206a
Bruno Tenório <tenoriobn@gmail.com>
2024-09-13 15:14:12 -0300

    Lidando com Generics types

--- src/utils/Armazenador.ts
+++ src/utils/Armazenador.ts
@@ -6,7 +6,7 @@ export class Armazenador {
     localStorage.setItem(chave, valorComoString);
   }
 
-  static obter(chave: string, reviver?: (this: any, key: string, value: any) => any) {
+  static obter<T>(chave: string, reviver?: (this: any, key: string, value: any) => any): T | null {
     const valor = localStorage.getItem(chave);
 
     if (valor === null) {
@@ -14,9 +14,9 @@ export class Armazenador {
     }
 
     if (reviver) {
-      return JSON.parse(valor, reviver)
+      return JSON.parse(valor, reviver) as T;
     }
 
-    return JSON.parse(valor);
+    return JSON.parse(valor) as T;
   }
 }
